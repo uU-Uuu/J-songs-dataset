61fabb8d5586e4543a5a4069654c22060a39c0d3
uU-Uuu <ustja.54@gmail.com>
2024-10-11 14:14:53 +0900

    started lex phr df kanji-hiragana-melody(full)-songid

--- code/melody/lyric_parser.py
+++ code/melody/lyric_parser.py
@@ -1,3 +1,4 @@
+from os import stat
 from lxml import etree
 from itertools import zip_longest
 import re
@@ -12,81 +13,36 @@ from sudachipy import tokenizer, dictionary
 parser = etree.XMLParser()
 
 xml_filename = 'xml\\1-ishikaribanka.xml'
-txt_filename = 'lyrics\\1-k-ishikaribanka.txt'
+txt_k_filename = 'lyrics\\1-k-ishikaribanka.txt'
 txt_h_filename = 'lyrics\\1-h-ishikaribanka.txt'
 
 
-def get_xml_root(filename):
 
-    with open(filename, encoding='utf-8', mode='r') as file:
-        xml_str = file.read()
-        xml_str = xml_str.encode('utf-8')
-        root = etree.fromstring(xml_str)
-
-    title = root.find('title').text
-    key = root.find('key').text
-    time_signature = root.find('time_signature').text
-
-    return root
-
-
-def get_mel_xml_lines(root):
-    mel_lines = []
-    for mel_phr in root.findall('.//melodic_phrase'):
-        lex_lines = []
-        lex_phrs = mel_phr.getchildren()
-        for lex_phr in lex_phrs:
-            lex_line = []
-            syllables = lex_phr.findall('.//syllable')
-            for syll in syllables:
-                lyrics, pitches, durations = tuple(map(lambda x: syll.findall(x), ('.//lyric', './/pitch', './/duration')))
-                lyrics = tuple(map(lambda el: el.text, lyrics))
-                pitches = tuple(map(lambda el: el.text, pitches))
-                durations = tuple(map(lambda el: el.text, durations))
-
-                lex_line.append(tuple(zip_longest(lyrics, pitches, durations)))
-            lex_lines.append(lex_line)
-        mel_lines.append(lex_lines)
-    return mel_lines
-
-
-
-def concat_lex_phrs_xml():
-    mel_lines = get_mel_xml_lines(get_xml_root(xml_filename))
-    lex_lines_annotated = []
-    for mel_line in mel_lines:
-        for lex_line in mel_line:
-            lex_line_syllables = []
-            for syllable in lex_line:
-                syllable_line = ''
-                for note in syllable:
-                    mora, pitch, duration = note
-                    if mora:
-                        syllable_line += f'{mora}={pitch}*{duration}'
-                    else:
-                        syllable_line += f'+{pitch}*{duration}'
-                lex_line_syllables.append(syllable_line)
-            lex_lines_annotated.append(' '.join(lex_line_syllables))
-    return lex_lines_annotated
-
-
-
-def zip_words_pitches():
-    with open(txt_h_filename, 'r', encoding='utf-8') as file:
-        lyr_phrs = file.readlines()
-        lex_phrs_annotated = concat_lex_phrs_xml()
-        zipped_ = list(zip_longest(lyr_phrs, lex_phrs_annotated))
-        pprint(zipped_)
         
-zip_words_pitches()
+# zip_words_pitches()
 
 class LyricsTokenizer:
     lyr_tokenizer = dictionary.Dictionary(dict='full').create()
     mode = tokenizer.Tokenizer.SplitMode.C
 
     @classmethod
-    def create_df(cls):
-        cls.df = pd.DataFrame(columns=['Token', 'Reading', 'Dict. form', 'POS', 'Line', 'Song ID'])
+    def create_word_df(cls):
+        cls.word_df = pd.DataFrame(columns=['Token', 'Reading', 'Dict. form', 'POS', 'Line', 'Song ID'])
+
+    @classmethod
+    def create_lex_ph_df(cls):
+        cls.lex_ph_df = pd.DataFrame(columns=['Lex. Phr.', 'Reading', 'Pitch accent', 'Melody full', 'Line', 'Song ID'])
+
+    @classmethod
+    def melody_file_to_df(cls, k_filename, h_filename, xml_filename):
+        zipped_, song_id = cls.zip_words_pitches(
+            xml_filename=xml_filename, h_filename=h_filename, k_filename=k_filename)
+        for phrase in zipped_:
+
+            phrase_info = [phrase[0], phrase[1].strip('\n'), '', phrase[2], '', song_id]
+            cls.lex_ph_df.loc[len(cls.lex_ph_df.index)] = phrase_info
+        display(cls.lex_ph_df)
+
 
     @classmethod
     def tokenize_phrase(cls, phrase, line_no, song_id):
@@ -97,12 +53,12 @@ class LyricsTokenizer:
             token = cls.lyr_tokenizer.tokenize(el, cls.mode)[0]
             token_info = [token.surface(), token.reading_form(), token.dictionary_form(), 
                           token.part_of_speech(), line_no, song_id]
-            cls.df.loc[len(cls.df.index)] = token_info
+            cls.df.loc[len(cls.word_df.index)] = token_info
 
     @classmethod
-    def tokenize_file(cls, filename):
-        with open(filename, 'r', encoding='utf-8') as file:
-            song_dir = filename.strip('../J-SONGS-DATASET/lyrics\\')
+    def tokenize_file(cls, k_filename):
+        with open(k_filename, 'r', encoding='utf-8') as file:
+            song_dir = k_filename.strip('../J-SONGS-DATASET/lyrics\\')
             match_song_id = re.match(r'^\d*', song_dir)
             song_id = match_song_id.group()
             line_no = 1
@@ -111,14 +67,96 @@ class LyricsTokenizer:
                 cls.tokenize_phrase(line, line_no, song_id)
                 line_no += 1
 
+    def get_xml_root(xml_filename):
+        with open(xml_filename, encoding='utf-8', mode='r') as file:
+            xml_str = file.read()
+            xml_str = xml_str.encode('utf-8')
+            root = etree.fromstring(xml_str)
+
+        title = root.find('title').text
+        key = root.find('key').text
+        time_signature = root.find('time_signature').text
+
+        return root
+
+    @staticmethod
+    def get_mel_xml_lines(root):
+        mel_lines = []
+        for mel_phr in root.findall('.//melodic_phrase'):
+            lex_lines = []
+            lex_phrs = mel_phr.getchildren()
+            for lex_phr in lex_phrs:
+                lex_line = []
+                syllables = lex_phr.findall('.//syllable')
+                for syll in syllables:
+                    lyrics, pitches, durations = tuple(map(lambda x: syll.findall(x), ('.//lyric', './/pitch', './/duration')))
+                    lyrics = tuple(map(lambda el: el.text, lyrics))
+                    pitches = tuple(map(lambda el: el.text, pitches))
+                    durations = tuple(map(lambda el: el.text, durations))
+
+                    lex_line.append(tuple(zip_longest(lyrics, pitches, durations)))
+                lex_lines.append(lex_line)
+            mel_lines.append(lex_lines)
+        return mel_lines
+
+
+    @staticmethod
+    def concat_lex_phrs_xml(xml_filename):
+        mel_lines = LyricsTokenizer.get_mel_xml_lines(
+            LyricsTokenizer.get_xml_root(xml_filename))
+        lex_lines_annotated = []
+        for mel_line in mel_lines:
+            for lex_line in mel_line:
+                lex_line_syllables = []
+                for syllable in lex_line:
+                    syllable_line = ''
+                    for note in syllable:
+                        mora, pitch, duration = note
+                        if mora:
+                            syllable_line += f'{mora}={pitch}*{duration}'
+                        else:
+                            syllable_line += f'+{pitch}*{duration}'
+                    lex_line_syllables.append(syllable_line)
+                lex_lines_annotated.append(' '.join(lex_line_syllables))
+        return lex_lines_annotated
+
+
+    @staticmethod
+    def parse_k_txt_file(k_filename):
+        with open(k_filename, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        lex_phrs = []
+        for line in lines:
+            lex_phrs.extend(line.split())
+        return lex_phrs
+
+    @staticmethod
+    def zip_words_pitches(xml_filename, h_filename, k_filename=None, ignore_blank=True):
+        with open(h_filename, 'r', encoding='utf-8') as file:
+            song_dir = h_filename.strip('../J-SONGS-DATASET/lyrics\\')
+            match_song_id = re.match(r'^\d*', song_dir)
+            song_id = match_song_id.group()
+            h_lyr_phrs = file.readlines()
+
+        if k_filename:
+            k_lyr_phrs = LyricsTokenizer.parse_k_txt_file(k_filename)
+
+        if ignore_blank:
+            h_lyr_phrs = list(filter(lambda phr: phr != '\n', h_lyr_phrs))
+            k_lyr_phrs = list(filter(lambda phr: phr != '\n', k_lyr_phrs))
+        lex_phrs_annotated = LyricsTokenizer.concat_lex_phrs_xml(xml_filename)
+
+        zipped_ = list(zip_longest(k_lyr_phrs, h_lyr_phrs, lex_phrs_annotated))
+        return zipped_, song_id
 
 
 pd.options.display.min_rows = 100
 
-# lyrics_tokenizer = LyricsTokenizer()
-# lyrics_tokenizer.create_df()
-# lyrics_tokenizer.tokenize_file(txt_filename)
+lyr_tokenizer = LyricsTokenizer()
+lyr_tokenizer.create_word_df()
+lyr_tokenizer.create_lex_ph_df()
+lyr_tokenizer.melody_file_to_df(txt_k_filename, txt_h_filename, xml_filename)
+# lyrics_tokenizer.tokenize_file(txt_k_filename)
 # display(lyrics_tokenizer.df)
 
-
-
+# lyr_tokenizer.parse_k_txt_file(txt_k_filename)
